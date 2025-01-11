dcfbe3a94ec6b45b71ca1c3b6841596658058c02
sebas1913 <sebasosornicastrillon@gmail.com>
2025-01-10 19:05:01 -0500

    Add styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,15 +18,26 @@ export default function Home() {
 
         } catch (error) {
             console.log(error);
-            
+
         }
     }
 
     return (
-        <div>
-            <h1>Hola</h1>
-            <button onClick={handlePhrase}>Search</button>
-            <p>{phrase}</p>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-[#254443] text-gray-800">
+            <h1 className="text-4xl font-extrabold mb-6 text-[#56EAAC]">
+                Phrase generator challenge
+            </h1>
+            <div className="flex items-center space-x-4 w-[500px] justify-between">
+                <p className="text-base font-normal text-[#56EAAC] truncate">
+                    {phrase}
+                </p>
+                <button
+                    onClick={handlePhrase}
+                    className="font-bold px-3 py-1 bg-[#56EAAC] text-[#254443] rounded-md text-sm"
+                >
+                    Buscar
+                </button>
+            </div>
         </div>
     );
 }
